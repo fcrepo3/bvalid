c65dc8a724d69c69b85a3da18e03d8722705f6fb
Chris Wilper <cwilper@duraspace.org>
2006-03-09 08:07:51 +0000

    started junit tests

---
+++ src/testjava/net/sf/bvalid/SchemaLanguageTest.java
@@ -0,0 +1,63 @@
+package net.sf.bvalid;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+public class SchemaLanguageTest extends TestCase {
+
+    public SchemaLanguageTest(String name) { super (name); }
+
+    public void setUp() {
+    }
+            
+    public void tearDown() {
+    }
+
+    //---------------------------------------------------------[ Test methods ]
+
+    public void testForNameXSD() throws ValidatorException {
+
+        SchemaLanguage xsdLower = SchemaLanguage.forName("xsd");
+        assertEquals(xsdLower, SchemaLanguage.XSD);
+
+        SchemaLanguage xsdUpper = SchemaLanguage.forName("XSD");
+        assertEquals(xsdUpper, SchemaLanguage.XSD);
+    }
+
+    public void testForNameUnrecognized() {
+        boolean recognized = false;
+        try {
+            SchemaLanguage.forName("unrecognized");
+            recognized = true;
+        } catch (ValidatorException e) {
+        } finally {
+            assertFalse(recognized);
+        }
+    }
+
+    public void testGetNameXSD() {
+        assertEquals(SchemaLanguage.XSD.getName(), "XSD");
+    }
+
+    public void testGetURIXSD() {
+        assertEquals(SchemaLanguage.XSD.getURI(), "http://www.w3.org/2001/XMLSchema");
+    }
+
+    public void testSupportedListContainsXSD() {
+
+        boolean foundXSD = false;
+        SchemaLanguage[] langs = SchemaLanguage.getSupportedList();
+        for (int i = 0; i < langs.length; i++) {
+            if (langs[i] == SchemaLanguage.XSD) {
+                foundXSD = true;
+            }
+        }
+
+        assertTrue(foundXSD);
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(SchemaLanguageTest.class);
+    }   
+
+}
